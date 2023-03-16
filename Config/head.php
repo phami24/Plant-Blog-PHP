1908ceb4e59c7e8cf68328a41165437fd3fe3bb7
MP24 <buibinhminhpham2004@gmail.com>
2023-03-16 10:55:03 +0700

    Thiết kế lại header

--- Config/head.php
+++ Config/head.php
@@ -16,11 +16,13 @@
     <!-- Icon -->
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+    <style>
+        <?php include '/xampp/htdocs/e-project1/Config/css/style.css' ?>
+    </style>
 </head>
 
 <body class="bg-light-subtle">
-    
-    <style><?php include'/xampp/htdocs/e-project1/Config/css/style.css' ?></style>
     <!-- Navbar -->
     <h1>Second Commit</h1>
     <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top d-flex" id="navbar">
@@ -34,7 +36,15 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav  mb-2 mb-lg-0 flex-wrap ms-auto me-auto ">
+                <ul class="navbar-nav  mb-2 mb-lg-0 flex-wrap  me-auto">
+                    <li class="nav-item dropdown mt-1 ">
+                        <a class="nav-link dropdown-toggle btn-md" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <ion-icon name="search-outline"></ion-icon>
+                        </a>
+                        <form class="nav-item  dropdown-menu">
+                            <input type="text" aria-describedby="basic-addon1" name="search" placeholder="Search" class="search">
+                        </form>
+                    </li>
                     <li class="nav-item">
                         <a class="nav-link " aria-current="page" href="#">
                             Trang chủ</a>
@@ -56,20 +66,21 @@
                         </ul>
                     </li>
                 </ul>
-                <div class="nav navbar-nav">
-                    <form class="nav-item input-group">
-                        <input type="text" aria-describedby="basic-addon1" name="search" placeholder="Search">
-                        <span class="input-group-text me-2" id="basic-addon1"><ion-icon name="search-outline"></ion-icon></span>
-                    </form>
-                </div>
-                <div class="nav navbar-nav me-auto ms-1">
-                    <div>
-                        <ion-icon class="nav-item ms-2 mt-2" name="call-outline" style="font-size: 25px"></ion-icon>
-                        <ion-icon class="nav-item ms-2 mt-2" name="mail-outline" style="font-size: 25px"></ion-icon>
-                        <ion-icon class="nav-item ms-2 mt-2" name="cart-outline" style="font-size: 25px"></ion-icon>
-                    </div>
-                </div>
-
+                <ul class="nav navbar-nav">
+                    <li class="nav-item">
+                        <div class=" btn-group ms-1 me-5 w-1">
+                            <a class="nav-link dropdown-toggle user " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+                                    <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z" />
+                                </svg>
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-md-end">
+                                <li><a class="dropdown-item" href="#">Login</a></li>
+                                <li><a class="dropdown-item" href="#">Reg</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </div>
     </nav>
