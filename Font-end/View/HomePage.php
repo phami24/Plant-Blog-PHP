2371238cc43292c7866afc6b7e8f8daf139b3c52
Toan Dv <toando2408@gmail.com>
2023-03-18 13:48:30 +0700

    doi ten file

--- Font-end/View/HomePage.php
+++ Font-end/View/HomePage.php
@@ -1,5 +1,7 @@
 <?php include "/xampp/htdocs/e-project1/Config/head.php" ?>
+<div class="container">
 
-<h1>Train Git</h1>
+    <h1>Train Git</h1>
+</div>
 
 <?php include "/xampp/htdocs/e-project1/Config/footer.php" ?>
