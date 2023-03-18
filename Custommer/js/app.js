4a93a5aaab8d0b21bb03149b5b0f408b82b9f763
MP24 <buibinhminhpham2004@gmail.com>
2023-03-18 20:43:15 +0700

    form reg/login not style

--- Custommer/js/app.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('register-link').addEventListener('click', function() {
-    document.getElementById('login-tab').classList.remove('show', 'active');
-    document.getElementById('register-tab').classList.add('show', 'active');
-  });
