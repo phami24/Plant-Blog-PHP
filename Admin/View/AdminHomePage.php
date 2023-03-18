38f7194ebc5029009f7e4dfed8892343b733485e
MP24 <buibinhminhpham2004@gmail.com>
2023-03-19 00:14:13 +0700

    admin page create form basic

--- Admin/View/AdminHomePage.php
+++ Admin/View/AdminHomePage.php
@@ -15,64 +15,15 @@
         include "C:/xampp/htdocs/e-project1/Lib/css/bootstrap.min.css";
         ?>
     </style>
-    <style>
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            user-select: none;
-        }
-
-        @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
-        }
-
-        .b-example-divider {
-            height: 3rem;
-            background-color: rgba(0, 0, 0, .1);
-            border: solid rgba(0, 0, 0, .15);
-            border-width: 1px 0;
-            box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-        }
-
-        .b-example-vr {
-            flex-shrink: 0;
-            width: 1.5rem;
-            height: 100vh;
-        }
-
-        .bi {
-            vertical-align: -.125em;
-            fill: currentColor;
-        }
-
-        .nav-scroller {
-            position: relative;
-            z-index: 2;
-            height: 2.75rem;
-            overflow-y: hidden;
-        }
-
-        .nav-scroller .nav {
-            display: flex;
-            flex-wrap: nowrap;
-            padding-bottom: 1rem;
-            margin-top: -1px;
-            overflow-x: auto;
-            text-align: center;
-            white-space: nowrap;
-            -webkit-overflow-scrolling: touch;
-        }
-    </style>
 
 </head>
 
 
 <body class="container">
-    <div class="row  justify-content-center  w-100 m-auto" style="margin-top:50%; ">
+    <div class="row  justify-content-center" style="margin-top:25%; ">
+        <div class="col-sm-4 col-md-6">
+            <img src="https://www.shutterstock.com/image-vector/man-inscription-admin-icon-outline-600w-1730974153.jpg" alt="">
+        </div>
         <div class="col-sm-8 col-md-6">
             <div class="tab-content">
                 <div class="tab-pane fade show active" id="login-tab">
