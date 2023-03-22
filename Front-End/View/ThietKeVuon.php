859d5bfe4351f33d927e6c2780cfbd5771de9ee6
tathanhthuy17032004@gmail.com <tathanhthuy17032004@gmail.com>
2023-03-22 10:25:34 +0700

    thietkevuon

--- Front-End/View/ThietKeVuon.php
+++ Front-End/View/ThietKeVuon.php
@@ -1,8 +1,156 @@
 <?php include"/xampp/htdocs/e-project1/Config/head.php" ?>
 
-<!--
-    Code here 
-    Tất cả bài viết về cách thiết kế vườn
--->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/Lib/css/bootstrap.min.css">
+  <title>Design</title>
+  <style>
+    div.text {
+      /* text-overflow: ellipsis; */
+      -webkit-line-clamp: 6;
+      /* -webkit-box-orient: vertical; */
+      overflow:auto;
+      /* display:none; */
+      /* position: absolute; */
+    }
+
+    .bg_img {
+      background-image: url("../img/20977137-green-leaf-isolated-background.webp");
+      height: 50%;
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+      position: sticky;
+    }
+
+    
+  </style>
+</head>
+
+<body>
+  <div class="bg_img">
+    <div class="container mt-5 mb-5">
+      <div class="container-fluid text-bg-dark mb-5 bg_img ">
+        <div class="pt-5 pb-4">
+          <p class=" font-monospace text-success h4 text-end">Người trồng cây là những người biết yêu thương người khác.</p>
+          <p class=" font-monospace text-success h4">Những chuyến đi về thiên nhiên là cách tái tạo năng lượng và giúp bạn khám phá những điều tuyệt vời trong tự nhiên.</p>
+        </div>
+      </div>
+
+      <div class="row mb-2 ">
+        <!-- hinh_1 -->
+        <div class="col-sm-6 col-md-4 mb-3">
+          <div class="card col" >
+            <a href="HomePage.php" class="card-link nav-link">
+              <img src="../img/design_01.jpg" alt="Design" class="card-img-top">
+              <div class="card-body text ">
+                <h4 class="card-title "style="min-height: 70px;">Ý Tưởng Thiết Kế Tuyệt Vời Cho Khu Vườn Nhỏ</h4>
+                <!-- <p class="card-text ">Sử dụng màu sắc và lặp lại để thêm chiều sâu cho khu vườn</p> -->
+              </div>
+            </a>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+        </div>
+        <!-- hinh_2 -->
+        <div class="col-sm-6 col-md-4 mb-3">
+          <div class="card col" >
+            <a href="" class="card-link nav-link">
+              <img src="../img/thiet-ke-vuon-bang-soi-da-03.jpg" alt="Design" class="card-img-top">
+              <div class="card-body text">
+                <h4 class="card-title "style="min-height: 70px;">Ý Tưởng Thiết Kế Bằng Sỏi Và Đá Cho Sân Vườn Nhà Bạn vbxgfkjhgfcvbghjhgf</h4>
+                <!-- <p class="card-text ">Bạn đang tìm hiểu về cách thiết kế vườn có sử dụng sỏi? Sỏi là</p> -->
+              </div>
+            </a>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+        </div>
+        <!-- hinh_3 -->
+        <div class="col-sm-6 col-md-4 mb-3">
+          <div class="card col" >
+            <a href="" class="card-link nav-link">
+              <img src="../img/design_01.jpg" alt="Design" class="card-img-top">
+              <div class="card-body text">
+                <h4 class="card-title "style="min-height: 70px;">Ý Tưởng Thiết Kế Tuyệt Vời Cho Khu Vườn Nhỏ</h4>
+                <!-- <p class="card-text ">Sử dụng màu sắc và lặp lại để thêm chiều sâu cho khu vườn</p> -->
+              </div>
+            </a>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+        </div>
+        <!-- hinh_4 -->
+        <div class="col-sm-6 col-md-4 mb-3 ">
+          <div class="card col" >
+            <a href="" class="card-link nav-link">
+              <img src="../img/design_01.jpg" alt="Design" class="card-img-top">
+              <div class="card-body text">
+                <h4 class="card-title "style="min-height: 70px;">Ý Tưởng Thiết Kế Tuyệt Vời Cho Khu Vườn Nhỏ</h4>
+                <!-- <p class="card-text ">Sử dụng màu sắc và lặp lại để thêm chiều sâu cho khu vườn</p> -->
+              </div>
+            </a>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container  mt-5 mb-5">
+      <h4 class="text-success">Thông tin liên quan:</h4>
+
+      <!-- hinh_1 -->
+      <div class="card mb-3">
+        <a href="" class="card-link nav-link">
+          <div class="row g-0">
+            <div class="col-sm-4">
+              <img src="../img/design_01.jpg" class="w-50" alt="...">
+            </div>
+            <div class="col-sm-8">
+              <div class="card-body">
+                <h5 class="card-title">Sen đá đô la trắng</h5>
+                <p class="card-text">Sen đá đô la trắng là một trong những loài sen có khả năng chịu được nắng mưa của môi trường nên cây dễ trồng và dễ chăm sóc; là sự lựa chọn ...</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <!-- hinh_2 -->
+      <div class="card mb-3">
+        <a href="" class="card-link nav-link">
+          <div class="row g-0">
+            <div class="col-sm-4">
+              <img src="../img/thiet-ke-vuon-bang-soi-da-03.jpg" class="w-50" alt="...">
+            </div>
+            <div class="col-sm-8">
+              <div class="card-body">
+                <h5 class="card-title">Sen đá đô la trắng</h5>
+                <p class="card-text">Sen đá đô la trắng là một trong những loài sen có khả năng chịu được nắng mưa của môi trường nên cây dễ trồng và dễ chăm sóc; là sự lựa chọn ...</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+  </div>
+</body>
+
+</html>
+
+
 
 <?php include"/xampp/htdocs/e-project1/Config/footer.php" ?>
