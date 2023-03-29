d123bff52e50f435da3999dc439f835411a5098d
conga2k4 <buibinhminhpham2004@gmail.com>
2023-03-30 04:42:06 +0700

    Done database basic

--- Front-End/View/CongCu.php
+++ Front-End/View/CongCu.php
@@ -1,157 +1,208 @@
+<?php
+include "/xampp/htdocs/e-project1/Config/head.php";
+include "/xampp/htdocs/e-project1/Config/conn.php";
+?>
+
+<div class="container ">
+    <?php
+    $sql1 = "SELECT * FROM post WHERE post_category_id = 5";
+    $result1 = mysqli_query($conn, $sql1);
+    $post = mysqli_fetch_assoc($result1);
+    ?>
+    <div class="text-center my-4">
+        <h1 style="color: #61c203;"><?php echo $post['title'] ?></h1>
+    </div>
+</div>
+<div class="">
+    <div class="content-side col-lg-8 col-md-8 col-sm-12 col-xs-12">
+        <div class="inner-content">
+            <aside class="toc">
+                <ul class="toc-list">
+                    <ul class="toc-list  is-collapsible">
+                        <?php
+                        $post_id =$post['post_id'];
+                        $sql = "SELECT * FROM topics WHERE post_id = '$post_id'";
+                        $result = mysqli_query($conn, $sql);
+                        if (mysqli_num_rows($result) > 0) {
+                            while ($topic = mysqli_fetch_assoc($result)) {
+                                $text = $topic['content'];
+                                if ($topic['topic_name'] != 'null') {
+                        ?>
+                                    <li>
+                                        <a href="#<?php echo $topic['topic_id']?>"><?php echo $topic['topic_name']; ?></a>
+                                    </li>
+                        <?php
+                                }
+                            }
+                        }
+                        ?>
+                    </ul>
+                </ul>
+            </aside>
+            <!---------------- nội dung -------------------->
 
 
-<?php include "/xampp/htdocs/e-project1/Config/head.php" ?>
+            <div class="row">
+                <div class="col-8">
+                    <?php
+                    $result = mysqli_query($conn, $sql);
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($topic = mysqli_fetch_assoc($result)) {
+                            $topicId = $topic['topic_id'];
+                            $sql1 = "SELECT * FROM topics_img WHERE topic_id = '$topicId'";
+                            $result1 = mysqli_query($conn, $sql1);
+                            $topic_img = mysqli_fetch_assoc($result1);
 
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/Lib/css/bootstrap.min.css">
-  <title>Design</title>
-  <style>
-    div.text {
-      /* text-overflow: ellipsis; */
-      -webkit-line-clamp: 6;
-      /* -webkit-box-orient: vertical; */
-      overflow: auto;
-      /* display:none; */
-      /* position: absolute; */
-    }
-    .bg_img {
-      background-image: url("../img/Monstera-slide-1400x525.jpg");
-      height: 50%;
-      background-position: center;
-      background-repeat: no-repeat;
-      background-size: cover;
-      position: sticky;
-    }
-  </style>
-
-</head>
-<body>
-  <div>
-    <div class="container mt-5 mb-5">
-      <div class="container-fluid text-bg-dark mb-3 bg_img ">
-        <div class="pt-5 pb-4">
-          <p class=" font-monospace text-success h4 text-center ">Người trồng cây là những người biết yêu thương người khác.</p>
-          <p class=" font-monospace text-success h4 text-center">Những chuyến đi về thiên nhiên là cách tái tạo năng lượng và giúp bạn khám phá những điều tuyệt vời trong tự nhiên.</p>
-        </div>
-      </div>
-      <div class="text-center mb-3 row">
-        <h3>Công cụ</h3>
-      </div>
-      <div class="row mb-2 ">
-        <!-- hinh_1 -->
-        <div class="col-sm-6 col-md-4 mb-3">
-          <a href="" class="card-link nav-link">
-            <div class="card col">
-              <img src="../img/Monstera-slide-1400x525.jpg" alt="Design" class="card-img-top">
-              <div class="card-body text ">
-                <h5 class="card-title " style="min-height: 80px; max-height:80px">Title</h5>
-
-              </div>
-              <div class="card-footer">
-                <small class="text-muted">...</small>
-              </div>
+                    ?>
+                            <?php if ($topic['topic_name'] != 'null') { ?>
+                                <h3 style="text-align: justify;">
+                                    <strong>
+                                        <span class="notranslate" id="<?php echo $topic['topic_id']?>"><?php echo $topic['topic_name'] ?></span>
+                                    </strong>
+                                </h3>
+                            <?php } ?>
+                            <p style="text-align: justify;">
+                                <?php echo nl2br($topic['content']) ?>
+                            </p>
+                            <?php if ($topic_img['img_url'] != 'null') { ?>
+                                <p style="text-align: justify;">
+                                    <span class="notranslate">
+                                        <img src="../../Admin/img/<?php echo $topic_img['img_url']; ?>" />
+                                    </span>
+                                </p>
+                            <?php } ?>
+                            <p style="text-align: center;">
+                                <span class="notranslate">
+                                    Xem thêm: <a href="#" target="_blank">title</a>
+                                </span>
+                            </p>
+                    <?php
+                        }
+                    }
+                    ?>
+                </div>
             </div>
-          </a>
-        </div>
-        <!-- hinh_2 -->
-        <div class="col-sm-6 col-md-4 mb-3">
-          <a href="" class="card-link nav-link">
-            <div class="card col">
-              <img src="../img/Monstera-slide-1400x525.jpg" alt="Design" class="card-img-top">
-              <div class="card-body text">
-                <h5 class="card-title " style="min-height: 80px; max-height:80px">Title</h5>
 
-              </div>
-              <div class="card-footer">
-                <small class="text-muted">...</small>
-              </div>
-            </div>
-          </a>
-        </div>
-        <!-- hinh_3 -->
-        <div class="col-sm-6 col-md-4 mb-3">
-          <a href="" class="card-link nav-link">
-            <div class="card col">
-              <img src="../img/Monstera-slide-1400x525.jpg" alt="Design" class="card-img-top">
-              <div class="card-body text">
-                <h5 class="card-title " style="min-height: 80px; max-height:80px">Title</h5>
+            <!---------------------------------------- kết thúc nội dung -------------------------------------->
 
-              </div>
-              <div class="card-footer">
-                <small class="text-muted">...</small>
-              </div>
-            </div>
-          </a>
-        </div>
-        <!-- hinh_4 -->
-        <div class="col-sm-6 col-md-4 mb-3 "><a href="" class="card-link nav-link">
-            <div class="card col">
-              <img src="../img/Monstera-slide-1400x525.jpg" alt="Design" class="card-img-top">
-              <div class="card-body text">
-                <h5 class="card-title " style="min-height: 80px; max-height:80px">Title</h5>
+            <!-- phần bên phải -->
+            <div class="col-4" style="height: auto !important; min-height: 0px !important;">
+                <aside class="sidebar shop-sidebar padd-left-20" style="height: auto !important;">
+                    <div class="widget search-box" style="height: auto !important;">
 
-              </div>
-              <div class="card-footer">
-                <small class="text-muted">...</small>
-              </div>
+                        <!-- thanh Search -->
+                        <div class="input-right">
+                            <p style="font-size:25px; color:#87c00c; font-weight:bold">Tìm kiếm thêm thông tin trên Yêu Trồng Cây</p>
+                            <form id="frmSearch" method="post" action="">
+                                <div class="input-group search-form" style="line-height: 60px;">
+                                    <input class="form-control" id="txtSearch" style="margin-top: 18.5px;" name="keyword" value="" type="text" placeholder='Tìm kiếm...'>
+                                    <span type="submit" onclick="getfocus()" class="input-group-text me-3" id="basic-addon1" style="margin-top: 18px; background-color: #61c203;">
+                                        <ion-icon name="search-outline"></ion-icon>
+                                    </span>
+                                </div>
+                            </form>
+                        </div>
+
+                        <!-- bài viết cùng danh mục -->
+
+                        <p style="font-size:30px">Bài viết cùng danh mục</p>
+                        <!-- bài viết 1 -->
+                        <article class="card mb-2">
+                            <a href="#" class="card-link nav-link ">
+                                <div class=" row">
+                                    <figure class=" col-sm-4">
+                                        <img alt="" src="../img/Monstera-soil-banner.jpg" class="w-1 mt-2 mx-2" style="border-radius: 5px;">
+                                    </figure>
+                                    <div class="col-sm-8">
+                                        <div class="card-body">
+                                            <p class="card-title">title</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </article>
+                        <!-- bài viết 2 -->
+                        <article class="card mb-2">
+                            <a href="#" class="card-link nav-link ">
+                                <div class=" row">
+                                    <figure class=" col-sm-4">
+                                        <img alt="" src="../img/Monstera-slide-1400x525.jpg" class="w-1 mt-2 mx-2" style="border-radius: 5px;">
+                                    </figure>
+                                    <div class="col-sm-8">
+                                        <div class="card-body">
+                                            <p class="card-title">title</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </article>
+
+                        <!-- Sản phẩm liên quan -->
+                        <div class="overlay-box ">
+                            <p style="font-size:30px">Sản phẩm liên quan</p>
+                            <figure class="image-box">
+                                <a href="#">
+                                    <img alt="" src="https://www.yeutrongcay.com/uploads/pages/Bac-ha-socola-Chocolate mint-01_1627225885.jpg" width=60 height=NaN />
+                                </a>
+                            </figure>
+
+                            <div class="lower-content">
+                                <a href="#" class="nav-link mx-3 px-3">
+                                    <p>Title </p>
+                                    <span class="fa flaticon-play-button-3">Chi tiết</span>
+                                </a>
+                            </div>
+
+                            <figure class="image-box">
+                                <a href="">
+                                    <img alt="" src="https://vn-live-01.slatic.net/p/67a2d417ba92251645325208dde0cba5.jpg" width=60 height=NaN>
+                                </a>
+                            </figure>
+
+                            <div class="lower-content">
+                                <a href="#" class="nav-link mx-3 px-3">
+                                    <p class=" card-link">Title</p>
+                                    <span class="fa flaticon-play-button-3">Chi tiết</span>
+                                </a>
+                            </div>
+
+                            <!---- Bài viết mới--------------->
+                            <p style="font-size:30px">Bài viết mới</p>
+                            <!-- bài viết 1 -->
+                            <article class="card mb-2">
+                                <a href="#" class="card-link nav-link ">
+                                    <div class=" row">
+                                        <figure class=" col-sm-4">
+                                            <img alt="" src="https://www.yeutrongcay.com/uploads/pages/la-bac-ha-va-rau-hung-lui_1640388750.jpg" class="w-1 mt-2 mx-2">
+                                        </figure>
+                                        <div class="col-sm-8">
+                                            <div class="card-body">
+                                                <p class="card-title">Title</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </article>
+                            <!-- bài viết 2 -->
+                            <article class="card mb-2">
+                                <a href="#" class="card-link nav-link ">
+                                    <div class=" row">
+                                        <figure class=" col-sm-4">
+                                            <img alt="" src="https://www.yeutrongcay.com/uploads/pages/la-bac-ha-va-rau-hung-lui_1640388750.jpg" class="w-1 mt-2 mx-2">
+                                        </figure>
+                                        <div class="col-sm-8">
+                                            <div class="card-body">
+                                                <p class="card-title">Title</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </article>
+                        </div>
+                    </div>
             </div>
-          </a>
+            </aside>
         </div>
-      </div>
     </div>
 
-    <div class="container  mt-5 mb-5">
-      <h4 class="text-success">Thông tin liên quan:</h4>
-
-      <!-- hinh_1 -->
-      <div class="card mb-3">
-        <a href="" class="card-link nav-link">
-          <div class="row g-0">
-            <div class="col-sm-4">
-              <img src="../img/Monstera-slide-1400x525.jpg" class="w-100" alt="...">
-            </div>
-            <div class="col-sm-8">
-              <div class="card-body">
-                <h5 class="card-title">Title</h5>
-                <p class="card-text">Text...</p>
-                <p class="card-text"><small class="text-muted">...</small></p>
-              </div>
-            </div>
-          </div>
-        </a>
-      </div>
-
-      <!-- hinh_2 -->
-      <div class="card mb-3">
-        <a href="" class="card-link nav-link">
-          <div class="row g-0">
-            <div class="col-sm-4">
-              <img src="../img/Monstera-slide-1400x525.jpg" class="w-100" alt="...">
-            </div>
-            <div class="col-sm-8">
-              <div class="card-body">
-                <h5 class="card-title">Title</h5>
-                <p class="card-text">Text...</p>
-                <p class="card-text"><small class="text-muted">...</small></p>
-              </div>
-            </div>
-          </div>
-        </a>
-      </div>
-    </div>
-
-  </div>
-</body>
-
-</html>
-
-
-
-<?php include"/xampp/htdocs/e-project1/Config/footer.php" ?>
+    <?php include "/xampp/htdocs/e-project1/Config/footer.php" ?>
---
+++ Front-End/View/LuaChonSanPham.php
@@ -0,0 +1,50 @@
+<?php
+include "/xampp/htdocs/e-project1/Config/head.php";
+?>
+<?php
+include "/xampp/htdocs/e-project1/Config/conn.php";
+$sql = 'SELECT * FROM post WHERE post_category_id = 3 AND status =1;';
+
+$result = mysqli_query($conn, $sql);
+?>
+
+
+<div class="bg_img">
+    <div class="container mt-5 mb-5">
+        <div class="container-fluid text-bg-dark mb-5 bg_img ">
+            <div class="pt-5 pb-4">
+                <p class=" font-monospace text-success h4 text-end">Người trồng cây là những người biết yêu thương người khác.</p>
+                <p class=" font-monospace text-success h4">Những chuyến đi về thiên nhiên là cách tái tạo năng lượng và giúp bạn khám phá những điều tuyệt vời trong tự nhiên.</p>
+            </div>
+        </div>
+
+        <div class="row mb-2 ">
+            <!-- Start PHP code -->
+            <?php
+            if (mysqli_num_rows($result) > 0) {
+                while ($post = mysqli_fetch_assoc($result)) {
+
+            ?>
+                    <div class="col-sm-6 col-md-4 mb-3" >
+                        <a href="../../Front-End/View/ChiTietBaiViet.php?id=<?php echo $post['post_id'] ?>" class="card-link nav-link" >
+                            <div class="card col">
+                                <img style="min-height: 250px; max-height:250px" src="../../Admin/img/<?php echo $post['post_img']; ?>" alt="img" class="card-img-top">
+                                <div class="card-body text">
+                                    <h4 class="card-title " style="min-height: 100px; max-height:100px"><?php echo $post['title'] ?></h4>
+                                    <!-- <p class="card-text ">Bạn đang tìm hiểu về cách thiết kế vườn có sử dụng sỏi? Sỏi là</p> -->
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+            <?php
+                }
+            }
+            ?>
+            <!-- End PHP code -->
+        </div>
+    </div>
+
+</div>
+
+
+<?php include "/xampp/htdocs/e-project1/Config/footer.php" ?>
